35d593567cd4ca027f137a4a62158cdaafbcdf29
GustavoJuk <gustavojuk09@gmail.com>
2022-03-24 18:28:06 -0300

    Game scene added

--- mini04.playground/Contents.swift
+++ mini04.playground/Contents.swift
@@ -1,3 +1,13 @@
-import UIKit
+//TODO: Documentação
 
-var greeting = "Hello, playground"
+import PlaygroundSupport
+import SpriteKit
+import CoreGraphics
+
+let skView = SKView(frame: CGRect(x: 0, y: 0, width: 700, height: 500))
+
+let gameScene = GameScene(size: skView.frame.size)
+
+skView.presentScene(gameScene)
+
+PlaygroundSupport.PlaygroundPage.current.liveView = skView
