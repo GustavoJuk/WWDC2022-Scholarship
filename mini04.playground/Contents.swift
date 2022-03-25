8bd8f8cb262c428808de5c9ccd6778920bf59485
GustavoJuk <gustavojuk09@gmail.com>
2022-03-25 16:17:28 -0300

    Assets added

--- mini04.playground/Contents.swift
+++ mini04.playground/Contents.swift
@@ -1,13 +1,15 @@
 //TODO: Documentação
 
 import PlaygroundSupport
-import SpriteKit
 import CoreGraphics
+import SpriteKit
 
 let skView = SKView(frame: CGRect(x: 0, y: 0, width: 700, height: 500))
 
 let gameScene = GameScene(size: skView.frame.size)
 
 skView.presentScene(gameScene)
+
+gameScene.scaleMode = .aspectFill
 
 PlaygroundSupport.PlaygroundPage.current.liveView = skView
--- mini04.playground/Sources/IntroScene.swift
+++ mini04.playground/Sources/IntroScene.swift
@@ -2,4 +2,72 @@ import SpriteKit
 
 public class GameScene: SKScene {
     
+    public override init(size: CGSize) {
+        super.init(size: size)
+    }
+    
+    func addBrainBackView() {
+        let backBrainNode = SKSpriteNode(imageNamed: "backBrain.png")
+        
+        backBrainNode.position = CGPoint(x: frame.midX, y: frame.midY)
+        backBrainNode.zPosition = 0
+        
+        addChild(backBrainNode)
+    }
+    
+    func addSeratoninView() {
+        let seratoninView = SKSpriteNode(imageNamed: "seratonin.png")
+
+        seratoninView.position = CGPoint(x: frame.midX - 75, y: frame.midY - 14)
+        seratoninView.zPosition = 1
+        
+        
+        addChild(seratoninView)
+        
+    }
+    
+    func addDopamineView() {
+        let dopamineView = SKSpriteNode(imageNamed: "dopamine.png")
+
+        dopamineView.position = CGPoint(x: frame.midX + 65, y: frame.midY - 60)
+        dopamineView.zPosition = 2
+        
+        addChild(dopamineView)
+        
+    }
+    
+    func addNoradrenalineView() {
+        let noradrenalineView = SKSpriteNode(imageNamed: "noradrenaline.png")
+
+        noradrenalineView.position = CGPoint(x: frame.midX + 22, y: frame.midY + 70)
+        noradrenalineView.zPosition = 3
+        
+        addChild(noradrenalineView)
+        
+    }
+    
+    func addBraiinFrontView() {
+        let frontBrainSide = SKSpriteNode(imageNamed: "frontBrain.png")
+        
+        frontBrainSide.position = CGPoint(x: frame.midX, y: frame.midY)
+        frontBrainSide.zPosition = 4
+        
+        addChild(frontBrainSide)
+        
+    }
+    
+    public override func didMove(to view: SKView) {
+        self.backgroundColor = SKColor.cyan
+        
+        addBrainBackView()
+        addSeratoninView()
+        addDopamineView()
+        addNoradrenalineView()
+        addBraiinFrontView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
