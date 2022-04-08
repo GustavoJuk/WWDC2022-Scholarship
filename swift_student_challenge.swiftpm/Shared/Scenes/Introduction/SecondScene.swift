64c94dee090051e131fc7efe6493fce1097e5c24
GustavoJuk <gustavojuk09@gmail.com>
2022-04-08 18:13:59 -0300

    minor change

--- swift_student_challenge.swiftpm/Shared/Scenes/Introduction/SecondScene.swift
+++ swift_student_challenge.swiftpm/Shared/Scenes/Introduction/SecondScene.swift
@@ -50,7 +50,7 @@ public class SecondScene: SKScene {
     
     private func addText() {
         article.text = "Much is discussed about depression and how terrible is... but, do you know what happens exactly?\n\nDepression is classified as a disease because of its symptoms, which affect not only the psychological area, but also the body, since chemical processes take place when a person feels this way.\n\nMost people have a stigma about this issue because it is not a visible thing. Also the same people who judge the victims do not know what is going on inside their minds and bodies."
-        article.position = CGPoint(x: myScene.frame.midX, y: myScene.frame.midY)
+        article.position = CGPoint(x: myScene.frame.midX, y: myScene.frame.midY * 0.5)
         article.fontSize = MAIN_BODY_SIZE_FONT
         article.preferredMaxLayoutWidth = myScene.frame.size.width - 70
         article.numberOfLines = 10
